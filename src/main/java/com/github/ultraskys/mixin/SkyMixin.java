518545b5688e63f67c482f8e89c7ce5953e1129c
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-08-27 17:54:30 +0800

    SkyMixin: Replace wild import with single class imports
    IntelliJ, stop

--- src/main/java/com/github/ultraskys/mixin/SkyMixin.java
+++ src/main/java/com/github/ultraskys/mixin/SkyMixin.java
@@ -6,7 +6,11 @@ import com.github.ultraskys.DaySky;
 import com.github.ultraskys.SharedData;
 import finalforeach.cosmicreach.world.Sky;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
