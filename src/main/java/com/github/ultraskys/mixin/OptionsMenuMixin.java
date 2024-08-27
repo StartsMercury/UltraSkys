54982ac4e1b0a732778b8617ffe3be09c7cf522c
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-08-27 17:02:48 +0800

    Update Stars Count Button into a Slider
    * Custom slider class
    * Play hover sound when knob changes

--- src/main/java/com/github/ultraskys/mixin/OptionsMenuMixin.java
+++ src/main/java/com/github/ultraskys/mixin/OptionsMenuMixin.java
@@ -1,5 +1,6 @@
 package com.github.ultraskys.mixin;
 
+import com.github.ultraskys.IntsSlider;
 import com.github.ultraskys.SharedData;
 import com.github.ultraskys.SkyExtensions;
 import finalforeach.cosmicreach.gamestates.GameState;
@@ -8,7 +9,6 @@ import finalforeach.cosmicreach.gamestates.OptionsMenu;
 import finalforeach.cosmicreach.ui.UIElement;
 import finalforeach.cosmicreach.world.Sky;
 import finalforeach.cosmicreach.world.World;
-import java.util.Arrays;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,38 +19,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public abstract class OptionsMenuMixin extends GameState {
     @Unique
-    private int numStars = SharedData.getNumStars();
+    private static final int[] SORTED_SUPPORTED_STAR_COUNTS = new int[]{50, 100, 200, 400, 600, 1000, 2000, 2500, 3000, 5000};
+
+    @Unique
+    private IntsSlider starsCountSlider;
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(GameState previousState, CallbackInfo ci) {
-        UIElement StarsButton = new UIElement(400.0F, -200.0F, 250.0F, 50.0F) {
-            private static final int[] SORTED_DISTANCES = new int[]{50, 100, 200, 400, 600, 1000, 2000, 2500, 3000, 5000};
-            private int index;
-            public void onCreate() {
-                super.onCreate();
-
-                // See docs about return
-                int index = Arrays.binarySearch(SORTED_DISTANCES, OptionsMenuMixin.this.numStars);
-                // RHS: min(~a, N - 1) = min(-a - 1, N - 1) = -1 + min(-a, N)
-                this.index = index >= 0 ? index : -1 + Math.min(-index, SORTED_DISTANCES.length);
-
-                this.updateText();
+        starsCountSlider = new IntsSlider(SORTED_SUPPORTED_STAR_COUNTS, 400.0F, -200.0F, 250.0F, 50.0F) {
+            protected void onValueUpdate(int value) {
+                this.setText("Stars Count: " + value);
             }
-            public void onClick() {
-                super.onClick();
-                this.index = (this.index + 1) % SORTED_DISTANCES.length;
-                this.updateText();
-            }
+
             public void updateText() {
-                int numStars = OptionsMenuMixin.this.numStars = SORTED_DISTANCES[this.index];
-                SharedData.setUpdated(true);
-                this.setText("Stars Count: " + numStars);
+                this.setText("Stars Count: " + this.getValue());
             }
         };
 
-        StarsButton.show();
+        starsCountSlider.setIndexAtOrBefore(SharedData.getNumStars());
+        starsCountSlider.show();
         OptionsMenu uiElements = (OptionsMenu)(Object) this;
-        uiElements.uiObjects.add(StarsButton);
+        uiElements.uiObjects.add(starsCountSlider);
 
         UIElement VibrentDay = new UIElement(400.0F, -125.0F, 250.0F, 50.0F) {
             public void onCreate() {
@@ -88,12 +77,13 @@ public abstract class OptionsMenuMixin extends GameState {
     public void switchAwayTo(GameState gameState) {
         super.switchAwayTo(gameState);
 
-        final var numStars = this.numStars;
+        final var numStars = this.starsCountSlider.getValue();
         if (numStars == SharedData.getNumStars()) {
             return;
         }
 
         SharedData.setNumStars(numStars);
+        SharedData.setUpdated(true);
 
         World world = InGame.world;
         if (world == null) {
